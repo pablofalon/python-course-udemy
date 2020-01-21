ba4e91d7df52fe35c5fcf1c1f9d5a5832f5753a8
pablofalon <pablofalon@gmail.com>
2020-01-21 19:39:38 -0300

    seccion3

---
+++ 03-User-Inputs/code.py
@@ -0,0 +1,6 @@
+nombreIngresado = input("ingrese nombre a procesar: ")
+saludo = f"hola {nombreIngresado}"
+saludo2= "en que puedo ayudarte {}?".format(nombreIngresado)
+
+print(saludo)
+print(saludo2)
